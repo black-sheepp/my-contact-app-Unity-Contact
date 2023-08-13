efa33ca35e0f38bd05fc4f65b371e0f46c003f4c
Shivam <shivamguptanitw@gmail.com>
2023-08-13 15:13:04 +0530

    Fontawesome install and ready to use. Logo modified

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import './App.css';
-import CreateNewContactButton from './Components/CreateNewContactButton';
-import Nav from './Components/Nav';
-import SearchBar from './Components/SearchBar';
+import "./App.css";
+import CreateNewContactButton from "./Components/CreateNewContactButton";
+import Nav from "./Components/Nav";
+import SearchBar from "./Components/SearchBar";
+
+
 
 function App() {
-  return (
-    <div className='contact-app-header'>
-      <Nav/>
-      <div className='ml-60 mr-60 mt-4 flex justify-between'>
-        <div>
-        <p className='text-black text-5xl font-bold'>Stay</p>
-        <p className='text-4xl font-bold text-[#7d2ae8]'>in touch</p>
-        </div>
-        <SearchBar/>
-        <CreateNewContactButton/>
-    </div>
-    </div>
-  );
+     return (
+          <div>
+               <div className='contact-app-header'>
+                    <Nav />
+                    <div className='ml-60 mr-60 mt-4 flex justify-between'>
+                         <div>
+                              <p className='text-black text-5xl font-bold'>Stay</p>
+                              <p className='text-4xl font-bold text-[#7d2ae8]'>in touch</p>
+                         </div>
+                         <SearchBar />
+                         <CreateNewContactButton />
+                    </div>
+               </div>
+          </div>
+     );
 }
 
 export default App;
--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faPhone} from '@fortawesome/free-solid-svg-icons'
 const Logo = () => {
   return (
-    <button className="logo-name">UnityContacts</button>
+    <button className="logo-name"> <FontAwesomeIcon icon={faPhone} size="xl" style={{color: "#00ff4c",}} /> UnityContacts</button>
   )
 }
 
